01ca89e733f7b724b6c531fab34de9106344db67
nicknt <nikolasderm@gmail.com>
2021-07-01 00:26:07 +0300

    change login and register

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include 'connection.php';
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -13,17 +13,31 @@ $pass = str_replace("'", "\"", $pass);
 $pass2 = $_POST['PasswordRepeat'];
 settype($pass2, "string");
 $pass2 = str_replace("'", "\"", $pass2);
-if ($pass == $pass2 && $pass != null) {
-    $salt = generateRandomString(5);
-    $token = generateRandomString(10);
-    $hashedpass = hash('sha256', $salt . $pass, false);
-    $sql = "INSERT INTO login (username, password, salt, token)  VALUES ('$user', '$hashedpass', '$salt', '$token')";
-    if ($conn->query($sql) === TRUE) {
-        header('Location: login.php');
+if ($user != null) {
+    $sql = "SELECT * FROM login WHERE username = '$user';";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $user = "";
+        $nameError = $passwordError = "was-validated";
+    } else {
+        if ($pass == $pass2 && $pass != null) {
+            $salt = generateRandomString(10);
+            $token = generateRandomString(12);
+            $hashedpass = hash('sha256', $salt . $pass, false);
+            $sql = "INSERT INTO login (username, password, salt, token)  VALUES ('$user', '$hashedpass', '$salt', '$token')";
+            if ($conn->query($sql) === TRUE) {
+                header('Location: login.php');
+            }
+        }else{
+            $passwordError = "was-validated";
+        }
     }
 }
 
-function generateRandomString($length = 10) {
+
+
+function generateRandomString($length = 10)
+{
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
@@ -46,15 +60,15 @@ function generateRandomString($length = 10) {
         </div>
         <hr>
         <div class="d-grid mb-2">
-            <div class="form-floating <?php echo $nameError?>">
+            <div class="form-floating <?php echo $nameError ?>">
                 <input type="text" class="form-control" id="floatingUsername" name="Username" placeholder="Name" value="<?php echo $user ?>" required>
                 <label for="floatingInput">Username</label>
             </div>
-            <div class="form-floating <?php echo $passwordError?>">
+            <div class="form-floating <?php echo $passwordError ?>">
                 <input type="password" class="form-control" id="floatingPassword" name="Password" placeholder="Password" required>
                 <label for="floatingPassword">Password</label>
             </div>
-            <div class="form-floating <?php echo $passwordError?>">
+            <div class="form-floating <?php echo $passwordError ?>">
                 <input type="password" class="form-control" id="floatingPasswordRepeat" name="PasswordRepeat" placeholder="Repeat" required>
                 <label for="floatingPasswordRepeat">Repeat</label>
             </div>
