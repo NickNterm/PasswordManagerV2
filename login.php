6c4c0cdf8270167900e8861d744cb205d2164b8b
nicknt <nikolasderm@gmail.com>
2021-06-30 22:34:11 +0300

    change login and start register

--- login.php
+++ login.php
@@ -11,7 +11,8 @@ $user = str_replace("'", "\"", $user);
 $pass = $_POST['Password'];
 settype($pass, "string");
 $pass = str_replace("'", "\"", $pass);
-echo("testt");
+$tokk = $_COOKIE["TestCookie"];
+echo("cookie is $tokk");
 if ($user != null) {
     echo($user);
     $sql = "SELECT * FROM login WHERE username = '$user';";
@@ -25,40 +26,43 @@ if ($user != null) {
                 $_SESSION['password'] = $pass;
                 $_SESSION['username'] = $user;
                 $_SESSION['salt'] = $salt;
+                setcookie("UserToken", $row["token"]);
                 header("Location: logedin");
             }else{
                 $passwordError = "was-validated"; 
             }
         }
     }else{
+        $user = "";
         $nameError = $passwordError = "was-validated"; 
     }
 }
-?>
 
+?>
 <link rel="stylesheet" href="login_style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <html>
-
 <body>
-    <form class="form-signin" action="" method="post" novalidate>
+    <form class="form-signin needs-validation" action="" method="post">
         <div>
             <h1 class="h3 mb-3 fw-normal">Welcome</h1>
         </div>
         <hr>
-        <div class="d-grid mb-3">
+        <div class="d-grid mb-2">
             <div class="form-floating <?php echo $nameError?>">
-                <input type="text" class="form-control" id="floatingUsername" name="Username" placeholder="Name" required>
+                <input type="text" class="form-control" id="floatingUsername" name="Username" placeholder="Name" value="<?php echo $user ?>" required>
                 <label for="floatingInput">Username</label>
             </div>
             <div class="form-floating <?php echo $passwordError?>">
                 <input type="password" class="form-control" id="floatingPassword" name="Username" placeholder="Password" required>
                 <label for="floatingPassword">Password</label>
             </div>
+            <div class="form-check mb-2">
+                <input class="form-check-input" type="checkbox" value="" id="rememberMeCheck">
+                <label class="form-check-label" for="rememberMeCheck">Remember Me</label>
+            </div>
             <button type="submit" class="btn btn-outline-primary">Login</button>
-        </div>
     </form>
 </body>
-
 </html>
