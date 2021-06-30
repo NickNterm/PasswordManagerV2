df975ff376bae175ce2975c33c6e6d9cc36361ad
nicknt <nikolasderm@gmail.com>
2021-07-01 00:16:00 +0300

    change login and register

--- login.php
+++ login.php
@@ -11,30 +11,34 @@ $user = str_replace("'", "\"", $user);
 $pass = $_POST['Password'];
 settype($pass, "string");
 $pass = str_replace("'", "\"", $pass);
-$tokk = $_COOKIE["TestCookie"];
-echo("cookie is $tokk");
+$tokk = $_COOKIE["UserToken"];
+echo ("cookie is $tokk");
 if ($user != null) {
-    echo($user);
+    echo ($user);
     $sql = "SELECT * FROM login WHERE username = '$user';";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         // output data of each row
         while ($row = $result->fetch_assoc()) {
             $salt = $row["salt"];
-            echo($salt);
+            echo ($salt);
             if (hash('sha256', $salt . $pass, false) === $row["password"]) {
                 $_SESSION['password'] = $pass;
                 $_SESSION['username'] = $user;
                 $_SESSION['salt'] = $salt;
-                setcookie("UserToken", $row["token"]);
+                if ($_POST['rememberMeCheck'] == 1) {
+                    setcookie("UserToken", $row["token"]);
+                }else{
+                    setcookie("UserToken", "");
+                }
                 header("Location: logedin");
-            }else{
-                $passwordError = "was-validated"; 
+            } else {
+                $passwordError = "was-validated";
             }
         }
-    }else{
+    } else {
         $user = "";
-        $nameError = $passwordError = "was-validated"; 
+        $nameError = $passwordError = "was-validated";
     }
 }
 
@@ -43,26 +47,28 @@ if ($user != null) {
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <html>
+
 <body>
     <form class="form-signin needs-validation" action="" method="post">
         <div>
-            <h1 class="h3 mb-3 fw-normal">Welcome</h1>
+            <h1 class="h3 mb-3 fw-normal d-flex justify-content-center">Welcome</h1>
         </div>
         <hr>
         <div class="d-grid mb-2">
-            <div class="form-floating <?php echo $nameError?>">
+            <div class="form-floating <?php echo $nameError ?>">
                 <input type="text" class="form-control" id="floatingUsername" name="Username" placeholder="Name" value="<?php echo $user ?>" required>
                 <label for="floatingInput">Username</label>
             </div>
-            <div class="form-floating <?php echo $passwordError?>">
-                <input type="password" class="form-control" id="floatingPassword" name="Username" placeholder="Password" required>
+            <div class="form-floating <?php echo $passwordError ?>">
+                <input type="password" class="form-control" id="floatingPassword" name="Password" placeholder="Password" required>
                 <label for="floatingPassword">Password</label>
             </div>
             <div class="form-check mb-2">
-                <input class="form-check-input" type="checkbox" value="" id="rememberMeCheck">
+                <input class="form-check-input" type="checkbox" value="1" name="rememberMeCheck" id="rememberMeCheck">
                 <label class="form-check-label" for="rememberMeCheck">Remember Me</label>
             </div>
             <button type="submit" class="btn btn-outline-primary">Login</button>
     </form>
 </body>
+
 </html>
---
+++ register.php
@@ -0,0 +1,65 @@
+<?php 
+include 'connection.php';
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$user = $_POST['Username'];
+settype($user, "string");
+$user = str_replace("'", "\"", $user);
+$pass = $_POST['Password'];
+settype($pass, "string");
+$pass = str_replace("'", "\"", $pass);
+$pass2 = $_POST['PasswordRepeat'];
+settype($pass2, "string");
+$pass2 = str_replace("'", "\"", $pass2);
+if ($pass == $pass2 && $pass != null) {
+    $salt = generateRandomString(5);
+    $token = generateRandomString(10);
+    $hashedpass = hash('sha256', $salt . $pass, false);
+    $sql = "INSERT INTO login (username, password, salt, token)  VALUES ('$user', '$hashedpass', '$salt', '$token')";
+    if ($conn->query($sql) === TRUE) {
+        header('Location: login.php');
+    }
+}
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+?>
+
+<link rel="stylesheet" href="register_style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<html>
+
+<body>
+    <form class="form-signin needs-validation" action="" method="post">
+        <div>
+            <h1 class="h3 mb-3 fw-normal d-flex justify-content-center">Register</h1>
+        </div>
+        <hr>
+        <div class="d-grid mb-2">
+            <div class="form-floating <?php echo $nameError?>">
+                <input type="text" class="form-control" id="floatingUsername" name="Username" placeholder="Name" value="<?php echo $user ?>" required>
+                <label for="floatingInput">Username</label>
+            </div>
+            <div class="form-floating <?php echo $passwordError?>">
+                <input type="password" class="form-control" id="floatingPassword" name="Password" placeholder="Password" required>
+                <label for="floatingPassword">Password</label>
+            </div>
+            <div class="form-floating <?php echo $passwordError?>">
+                <input type="password" class="form-control" id="floatingPasswordRepeat" name="PasswordRepeat" placeholder="Repeat" required>
+                <label for="floatingPasswordRepeat">Repeat</label>
+            </div>
+            <button type="submit" class="btn btn-outline-primary">Register</button>
+    </form>
+</body>
+
+</html>
